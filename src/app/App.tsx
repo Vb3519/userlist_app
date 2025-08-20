d6bd8fba49339eafedb5da204c4f5ff5c9c400d5
vb3519 <vb415@bk.ru>
2025-08-20 18:55:51 +0300

    стартовая сборка

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-amber-600 text-center">Hello from React ^_^</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/repo_name/',
+  server: {
+    proxy: {},
+    watch: {},
+  },
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@shared': '/src/shared',
+      '@types': '/src/types',
+    },
+  },
+});
